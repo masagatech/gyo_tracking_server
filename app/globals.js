8f578941c8aad81fd711ac0cded8add7ca89acb3
llc <pratikway.90@gmail.com>
2017-07-10 21:46:40 +0530

    changes

--- app/globals.js
+++ app/globals.js
@@ -11,5 +11,5 @@ global.schema = function schema(params) {
 global.constr = function constr() {
     // return 'postgres://postgres:123@192.168.1.107:5432/goyo_marketing';
 
-    return 'postgres://postgres:sa@123@35.154.230.244:5432/goyo_track';
+    return 'postgres://postgres:sa@123@traveltrack.goyo.in:5432/goyo_traveltrack';
 };
