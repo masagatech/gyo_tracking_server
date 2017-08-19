c643f33c9f5a3bc66b8d8b243cf16ea5161ea017
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-08-19 22:04:05 +0530

    service committed

--- app/appmodule/schoolapi/fileupload.js
+++ app/appmodule/schoolapi/fileupload.js
@@ -4,9 +4,14 @@ var express = require('express');
 var path = require('path');
 var formidable = require('formidable');
 var fileupload = module.exports = {};
+// Include the node file module
+var Jimp = require("jimp");
+// Include ImageMagick
 
 fileupload.uploadFile = function uploadFile(req, res, done) {
+    let width = req.query.width || 'noresize';
     var form = new formidable.IncomingForm();
+
     form.uploadDir = "www/uploads"; //set upload directory
     form.keepExtensions = true; //keep file extension
 
@@ -18,13 +23,20 @@ fileupload.uploadFile = function uploadFile(req, res, done) {
     })
 
     form.on('file', function(field, file) {
+        if (width !== 'noresize') {
+            Jimp.read(file.path, function(err, lenna) {
+                if (err) throw err;
+                lenna.resize(Number(width), Jimp.AUTO) // resize 
+                    .quality(80) // set greyscale 
+                    .write(file.path); // save 
+            });
+        }
         files.push([field, file]);
     })
 
     form.on('end', function() {
         res.writeHead(200, { 'content-type': 'text/plain' });
         var sortedFiles = [];
-
         for (var i = 0; i <= files.length - 1; i++) {
             var upl = files[i][1];
 
