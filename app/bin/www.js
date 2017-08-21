1023dc642f8b8991e44c7f4e6eca7692c00405f0
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-08-22 01:41:35 +0530

    service is committed

--- app/bin/www.js
+++ app/bin/www.js
@@ -45,6 +45,8 @@ app.all('/*', function(req, res, next) {
 
 app.use('/images', express.static(__dirname.replace('app', "www").replace('bin', 'uploads')));
 
+app.use('/images', express.static(__dirname.replace('app', "www").replace('bin', 'mobile')));
+
 app.use(jwt({
     secret: 'abcd',
     credentialsRequired: false
